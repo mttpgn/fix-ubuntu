16de1c632d9e73bd731e021fe1742ea1a1175c4d
Matt Pagan <matthew.a.pagan@gmail.com>
2013-09-19 03:37:37 +0000

    Update fix-ubuntu-stb.sh

--- fix-ubuntu-stb.sh
+++ fix-ubuntu-stb.sh
@@ -14,6 +14,29 @@ fix_ubuntu(){
         echo 'Type=Application' >> $bashlauncher
         echo 'StartupNotify=true' >> $bashlauncher
         echo 'Path=/bin/bash' >> $bashlauncher
+        chmod +x $bashlauncher
     fi
 }
 fix_ubuntu
+
+## Ubuntu has these *.desktop files that create 'launchers' for applications. 
+## An application needs a launcher for it to be the default application for a filetype. 
+## Bash doesn't have one. That's why you can't run shell scripts in Ubuntu from the desktop.
+##
+## One workaround is to add a bash launcher in the ~/.local/share/applications directory. 
+## Call it 'bash.desktop'. Basically it should look something like this:
+##
+## [Desktop Entry]
+## Version=1.0
+## Type=Application
+## Name=Bash
+## Icon=bash
+## Exec=/bin/bash %U
+## Terminal=false
+## Path=/bin/bash
+##
+## After you've made the file, run 
+##
+## chmod +x bash.desktop
+##
+## to make the file executable. 
